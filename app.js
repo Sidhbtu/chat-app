02adc5434b5927926cf2eac718f2c4e715002057
Sudhanshu Arya <123s.arya456@gmail.com>
2020-10-22 01:11:44 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+let mapping = {}
+
+app.use('/',express.static(__dirname+'/static'))
+
+io.on('connection',(socket)=>{
+    console.log('Connection Established '+socket.id)
+    socket.emit('successfull',{
+        msg:"Connected Succcessfully"
+    })
+    socket.on('sending-msg',(data)=>{
+        console.log(socket.id + ' says '+ data.msg)
+        // io.emit, socket.emit
+        socket.broadcast.emit('recieved-msg',{
+            msg:data.msg,
+            name:mapping[socket.id] 
+        })
+    })
+    
+    socket.on('login',(data)=>{
+        mapping[socket.id] = data.name
+        socket.emit('login-success')
+    })
+})
+
+server.listen(4444,()=>{
+    console.log('Server started at http://localhost:4444')
+})
---
+++ static/script.js
@@ -0,0 +1,32 @@
+const socket = io()
+
+setTimeout(()=>{
+    console.log('Connected '+socket.id)
+},1000)
+
+$(()=>{
+    $('#chat').hide()
+
+    $('#btn1').click(()=>{
+        socket.emit('login',{
+            name:$('#inp1').val()
+        })
+
+    })
+
+    $('#btn').click(()=>{
+        socket.emit('sending-msg',{
+            msg:$('#inp').val()
+        })
+    })
+    socket.on('recieved-msg',(data)=>{
+        $('#list').append(
+            $('<li>').text(data.name + ' said :-' +  data.msg )
+        )
+    })
+
+    socket.on('login-success',()=>{
+        $('#login').hide()
+        $('#chat').show()
+    })
+})
